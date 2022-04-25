415fbe61b0df48853c84fa117fb9f0f9244436d5
Sabrina Gomes <sabrinagomes3@gmail.com>
2022-04-25 18:00:34 -0300

    criando class Livro

--- primeiro-projeto-IntelliJ/src/br/com/dio/PrimeiroPrograma.java
+++ primeiro-projeto-IntelliJ/src/br/com/dio/PrimeiroPrograma.java
@@ -6,8 +6,48 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro1 = new Livro("Do delito e das Penas", 128 );
+        System.out.println(livro1);
+
 //        int a = 5;
 //        int b = 3;
 //        System.out.println("Hello World!: " + (a+b));
     }
 }
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+
+
+}
+
