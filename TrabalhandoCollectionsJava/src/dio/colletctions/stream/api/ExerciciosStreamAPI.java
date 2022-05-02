cc994c4f709999f40e639fc9970666ca15265f63
Sabrina Gomes <sabrinagomes3@gmail.com>
2022-05-02 14:15:44 -0300

    Exercício aula StreamAPI

--- TrabalhandoCollectionsJava/src/dio/colletctions/stream/api/ExerciciosStreamAPI.java
+++ TrabalhandoCollectionsJava/src/dio/colletctions/stream/api/ExerciciosStreamAPI.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 
 public class ExerciciosStreamAPI {
@@ -28,6 +30,70 @@ public class ExerciciosStreamAPI {
 
 
 
+        System.out.println("\nPegue os números pares e maiores que 2 e coloque em uma lista:");
+
+        List<Integer>ListParesMaioresQue2 = numerosAleatorios.stream()
+                .map(Integer::parseInt)
+                .filter(i ->i % 2 == 0 && i > 2).collect(Collectors.toList());
+        System.out.println(ListParesMaioresQue2);
+
+        System.out.println("Mostre a média dos números: ");
+
+        numerosAleatorios.stream()
+                .mapToInt(Integer::parseInt)
+                .average()
+                .ifPresent(System.out::println);
+
+        System.out.println("Remova os valores ímpares: ");
+
+
+
+        System.out.println("Remova os valores ímpares: ");
+//        numerosAleatorios1.removeIf(integer -> integer % 2 != 0);
+//        System.out.println(numerosAleatorios1);
+//        numerosAleatoriosInteger.removeIf(i -> (i % 2 != 0));
+//        System.out.println(numerosAleatoriosInteger);
+
+//        Para você
+//        System.out.println("Ignore os 3 primeiros elementos da lista e imprima o restante:");
+//        numerosAleatoriosInteger.stream()
+//                .skip(3)
+//                .forEach(System.out::println);
+//
+//        long countNumerosUnicos = numerosAleatoriosInteger.stream()
+//                .distinct()
+//                .count();
+//        System.out.println("Retirando os números repetidos da lista, quantos números ficam? " + countNumerosUnicos);
+//
+//        System.out.print("Mostre o menor valor da lista: ");
+//        numerosAleatoriosInteger.stream()
+//                .mapToInt(Integer::intValue)
+//                .min()
+//                .ifPresent(System.out::println);
+//
+//        System.out.print("Mostre o maior valor da lista: ");
+//        numerosAleatoriosInteger.stream()
+//                .mapToInt(Integer::intValue)
+//                .max()
+//                .ifPresent(System.out::println);;
+//
+//        int somaDosNumerosPares = numerosAleatoriosInteger.stream()
+//                .filter(i -> (i % 2 == 0))
+//                .mapToInt(Integer::intValue)
+//                .sum();
+//        System.out.println("Pegue apenas os números ímpares e some: " + somaDosNumerosPares);
+//
+//        System.out.println("Mostre a lista na ordem númerica: ");
+//        List<Integer> numerosOrdemNatural = numerosAleatoriosInteger.stream()
+//                .sorted(Comparator.naturalOrder())
+//                .collect(Collectors.toList());
+//        System.out.println(numerosOrdemNatural);
+//
+//        System.out.println("Agrupe os valores ímpares múltiplos de 3 ou de 5:");
+////        dica: collect(Collectors.groupingBy(new Function())
+//        Map<Boolean, List<Integer>> collectNumerosMultiplosDe3E5 = numerosAleatoriosInteger.stream()
+//                .collect(Collectors.groupingBy(i -> (i % 3 == 0 || i % 5 == 0)));
+//        System.out.println(collectNumerosMultiplosDe3E5);
 
     }
 }
